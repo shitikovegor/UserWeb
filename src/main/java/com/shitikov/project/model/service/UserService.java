f25c4e877b48a25e0687196a7f185bc400fe6de1
shitikovegor <shitikov.egor@gmail.com>
2020-09-17 17:23:40 +0300

    refactoring

--- src/main/java/com/shitikov/project/model/service/UserService.java
+++ src/main/java/com/shitikov/project/model/service/UserService.java
@@ -9,10 +9,10 @@ import java.util.Optional;
 
 public interface UserService {
     boolean add(String login, String password, RoleType role) throws ServiceException;
-    boolean remove(String login);
-    boolean removeById(String id);
-    Optional<User> findById(String id);
-    List<User> findAll();
+    boolean remove(String login) throws ServiceException;
+    boolean removeById(String id) throws ServiceException;
+    Optional<User> findById(String id) throws ServiceException;
+    List<User> findAll() throws ServiceException;
     boolean checkLogin(String login, String password) throws ServiceException;
-    boolean updatePassword(String newPassword);
+    boolean updatePassword(String newPassword) throws ServiceException;
 }
